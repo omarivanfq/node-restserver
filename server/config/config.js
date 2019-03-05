ed4440bc94a78d233cbf539cc8a9fcda37394741
omarivanfq <omarivanfq@gmail.com>
2019-03-04 23:18:28 -0600

    Config NODE_ENV fixes

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,6 @@ process.env.PORT = process.env.PORT || 3000;
 //  Entorno
 // ============================
 
-console.log( " > > > > > > > > NODE_DEV = " + process.env.NODE_DEV + "  < < < < < < < < <")
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ============================
@@ -17,11 +16,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    console.log( " > > > > > > > > A < < < < < < < < <")
     urlDB = 'mongodb://localhost:27017/cafe';
 }
 else {
-    console.log( " > > > > > > > > B < < < < < < < < < " + process.env.MONGO_URI)
     urlDB = process.env.MONGO_URI;
 }
 

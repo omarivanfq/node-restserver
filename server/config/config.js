2f82a36bd66d84c8ff7c5a7a971da28649636ee0
omarivanfq <omarivanfq@gmail.com>
2019-03-04 22:25:54 -0600

    config changes

--- server/config/config.js
+++ server/config/config.js
@@ -14,11 +14,11 @@ process.env.NODE_DEV = process.env.NODE_DEV || 'dev';
 // ============================
 
 let urlDB;
-/*
+
 if (process.env.NODE_DEV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 }
-else {*/
+else {
     urlDB = 'mongodb+srv://omarivan:EZ2CoRANf0z6v9e3@cluster0-zrzaj.mongodb.net/cafe'
-//}
+}
 process.env.URLDB = urlDB
